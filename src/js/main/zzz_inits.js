3dc3c6266dd55781ce54efa81cb65a277e9a2a2e
cferdinandi <chris@gomakethings.com>
2016-11-17 15:05:43 -0500

    Updates

--- src/js/main/zzz_inits.js
+++ src/js/main/zzz_inits.js
@@ -8,3 +8,26 @@ fluidvids.init({
 	selector: ['iframe', 'object'],
 	players: ['www.youtube.com', 'player.vimeo.com', 'www.slideshare.net', 'www.google.com/maps', 'maps.google.com']
 });
+
+// Add show/hide password checkbox
+;(function (window, document, undefined) {
+
+	'use strict';
+
+	// Check that password fields exists
+	var pws = document.querySelector( '.wpwebapp-form-password' );
+	if ( !pws ) return;
+
+	// Get submit button
+	var submit = document.querySelector( '.wpwebapp-form-button' );
+	if ( !submit ) return;
+
+	// Inject password toggle
+	var div = document.createElement('div');
+	div.innerHTML = '<p>x</p><label class="x-ray"><input type="checkbox" data-x-ray=".wpwebapp-form-password" data-default="hide"> Show password</label>';
+	submit.parentNode.insertBefore( div.childNodes[1], submit );
+
+	// Initialize X-Ray
+	xray.init();
+
+})(window, document);
